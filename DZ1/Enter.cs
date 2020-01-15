8c95cbbc2114c0cf8e18cc520c1643c4361ad381
Andrew Muk <amuk89@mail.ru>
2020-01-15 23:41:18 +0700

    vopros

---
+++ DZ1/Enter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    class Enter
+    {
+        public string result;
+        public Enter(string question) 
+        {
+
+            Console.Write(question);
+            result = Console.ReadLine();
+
+        }
+        
+    }
+}
---
+++ DZ1/person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ1
+{
+    
+    
+    class person
+    {
+        public string name;
+
+        Enter _name = new Enter("Ваше имя: ");
+        name = _name.result;
+
+
+        Enter _last_name = new Enter("Ваша фамилия: ");
+        Enter _age = new Enter("Ваш возраст: ");
+        Enter _height = new Enter("Ваш рост: ");
+        Enter _weight = new Enter("Ваш вес: ");
+               
+    }
+}
